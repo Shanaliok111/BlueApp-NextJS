6a797c477c791e0e221af7b19f472377cbd076e1
Shanaliok111 <shanaliok111@gmail.com>
2025-12-03 02:07:26 +0500

    Add files via upload
    BlueApp core files added

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{fontFamily:'sans-serif',padding:'40px'}}>
+      <h1>BlueApp Web</h1>
+      <p>Deployment successful.</p>
+    </div>
+  );
+}
