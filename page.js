b2b5a0305b5d1caa9715ee4f79f12a93d2390050
Shanaliok111 <shanaliok111@gmail.com>
2025-12-03 15:24:56 +0500

    Remove root page component from page.js
    Removed the root page component from page.js.

--- page.js
+++ page.js
@@ -1,8 +1,1 @@
-export default function Home() {
-  return (
-    <div style={{fontFamily:'sans-serif',padding:'40px'}}>
-      <h1>BlueApp Web</h1>
-      <p>Deployment successful.</p>
-    </div>
-  );
-}
+remove root page.js
