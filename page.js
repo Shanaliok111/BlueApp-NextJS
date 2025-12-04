d73a2bd217ee1a5e372362947a0a716d5245dd66
Shanaliok111 <shanaliok111@gmail.com>
2025-12-04 16:33:06 +0500

    Add Home component for successful deployment message

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>BlueApp Deployed Successfully</h1>
+      <p>Your project is now connected to Vercel.</p>
+    </div>
+  );
+}
